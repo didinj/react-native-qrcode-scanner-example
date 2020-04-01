97605c7626a46fbba36d517e2747713d9fe90a83
didin jamaludin <didinj@gmail.com>
2020-04-01 08:06:14 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,144 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  Button
+} from 'react-native';
+
+import {
+  Header,
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
+const App = () => {
+
+  const [scan, setScan] = useState(false)
+  const [result, setResult] = useState()
+
+  onSuccess = (e) => {
+    setResult(e.data)
+    setScan(false)
+  }
+
+  startScan = () => {
+    setScan(true)
+    setResult()
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          <View style={styles.body}>
+            { result &&
+              <View style={styles.sectionContainer}>
+                <Text style={styles.centerText}>{result}</Text>
+              </View>
+            }
+            { !scan &&
+              <View style={styles.sectionContainer}>
+                <Button
+                  title="Start Scan"
+                  color="#f194ff"
+                  onPress={this.startScan}
+                />
+              </View>
+            }
+            { scan &&
+              <View style={styles.sectionContainer}>
+                <QRCodeScanner
+                  reactivate={true}
+                  showMarker={true}
+                  ref={(node) => { this.scanner = node }}
+                  onRead={this.onSuccess}
+                  topContent={
+                    <Text style={styles.centerText}>
+                      Scan your QRCode!
+                    </Text>
+                  }
+                  bottomContent={
+                    <TouchableOpacity style={styles.buttonTouchable} onPress={() => setScan(false)}>
+                      <Text style={styles.buttonText}>Cancel Scan</Text>
+                    </TouchableOpacity>
+                  }
+                />
+              </View>
+            }
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# React Native Tutorial: QRCode Scanner App for Android or iOS
+
+This source code is part of [React Native Tutorial: QRCode Scanner App for Android or iOS]()
